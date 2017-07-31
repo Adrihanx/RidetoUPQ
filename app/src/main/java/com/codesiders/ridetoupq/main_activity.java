b1052875b70c02b934edfcfcd83157822d53499a
EdwingRenteria <ed.rente.ga@gmail.com>
2017-07-30 20:44:52 -0500

    Cambio de boton de hello world a bienvenidos

--- app/src/main/java/com/codesiders/ridetoupq/main_activity.java
+++ app/src/main/java/com/codesiders/ridetoupq/main_activity.java
@@ -13,20 +13,19 @@ import android.widget.TextView;
  */
 
 public class main_activity extends AppCompatActivity {
-     Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        siguiente=(Button)findViewById(R.id.btn1);
-        siguiente.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
 
-                Intent i=new Intent(main_activity.this, clase_bienvenidos.class);
-                startActivity(i);
+        setContentView(R.layout.content_main);
+
+        Button btn = (Button) findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), clase_bienvenidos.class);
+                startActivityForResult(intent, 0);
             }
         });
 
